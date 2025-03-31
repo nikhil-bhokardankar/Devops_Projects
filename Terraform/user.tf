417044760c34df4cde857977f938af80882f97d2
Nikhil <nikhil.bhokardankar1303@gmail.com>
2025-03-31 17:37:58 +0530

    Added to dev branch

---
+++ Terraform/user.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_user" "demo" {
+  name = "awsuser1"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_access_key" "demo" {
+  user = aws_iam_user.demo.name
+}
+
+data "aws_iam_policy_document" "demo_ro" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "demo_ro" {
+  name   = "test"
+  user   = aws_iam_user.demo.name
+  policy = data.aws_iam_policy_document.demo_ro.json
+}
