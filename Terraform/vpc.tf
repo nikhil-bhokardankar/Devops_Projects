1b6e51c8488b56bd0c47212e5f6332fa3066ffc3
Nikhil <nikhil.bhokardankar1303@gmail.com>
2025-04-05 13:35:31 +0530

    subnet added

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -1,7 +1,24 @@
 resource "aws_vpc" "Tera-Demo-Vpc" {
-    cidr_block = "15.1.0.0/24"
-    tags = {
-      Name = "Tera-Demo-Vpc"
-    }
-  
+  cidr_block = "15.1.0.0/24"
+  tags = {
+    Name = "Tera-Demo-Vpc"
+  }
+
+}
+
+resource "aws_subnet" "Tera-Public-Subnet" {
+  vpc_id     = aws_vpc.Tera-Demo-Vpc.id
+  cidr_block = "15.1.0.0/25"
+  tags = {
+    Name = "Tera-Public-Subnet"
+  }
+}
+
+resource "aws_subnet" "Tera-Private-Subnet" {
+  vpc_id     = aws_vpc.Tera-Demo-Vpc.id
+  cidr_block = "15.1.0.128/25"
+  tags = {
+    Name = "Tera-Private-Subnet"
+  }
+
 }
